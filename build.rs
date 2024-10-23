7bc74f58ad338a981f736a87fe6d53c612663d68
Andrzej Hunt <andrzej@ahunt.org>
2024-10-23 06:57:12 +0200

    Rename to libp8020
    This code should probably work with the 8020 too.

--- build.rs
+++ build.rs
@@ -6,5 +6,5 @@ fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     cbindgen::generate(crate_dir)
         .expect("Unable to generate bindings")
-        .write_to_file("libp8020a.h");
+        .write_to_file("libp8020.h");
 }
