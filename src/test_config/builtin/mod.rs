eb554474c5896ad605238cd5a16430d9058ea305
Andrzej Hunt <andrzej@ahunt.org>
2024-11-09 12:18:02 +0100

    Add first set of builtin test configs (OSHA CNC's, Crash2.5)
    ISO protocol(s) should also be added at some point.
    
    Note: exercise naming is taken directly from the OSHA appendix. The modified
    protocols like to use a lot of hyphens, the original CNC protocol doesn't.
    Who am I to criticise the inconsistency?

---
+++ src/test_config/builtin/mod.rs
@@ -0,0 +1,5 @@
+pub const OSHA: &str = include_str!("osha.csv");
+pub const OSHA_LEGACY: &str = include_str!("osha_legacy.csv");
+pub const OSHA_FAST_FFP: &str = include_str!("osha_fast_ffp.csv");
+pub const OSHA_FAST_ELASTO: &str = include_str!("osha_fast_elasto.csv");
+pub const CRASH_2_5: &str = include_str!("crash_2_5.csv");
