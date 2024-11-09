3483e1412f93aa0cccc8e8e0c5301bf0a4b8ec64
Andrzej Hunt <andrzej@ahunt.org>
2024-11-09 15:52:43 +0100

    Tweak cbuildgen: don't fail build on syntax errors
    As suggested at:
    https://github.com/mozilla/cbindgen/blob/master/docs.md#buildrs
    
    Failing the entire build is annoying, because these failures are usually due to
    mistakes that will cause the build to fail anyway, and rustc provides a clearer
    explanation of what went wrong.

--- build.rs
+++ build.rs
@@ -5,6 +5,13 @@ use std::env;
 fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     cbindgen::generate(crate_dir)
-        .expect("Unable to generate bindings")
-        .write_to_file("libp8020.h");
+        .map_or_else(
+            |error| match error {
+                cbindgen::Error::ParseSyntaxError { .. } => {}
+                e => panic!("{:?}", e),
+            },
+            |bindings| {
+                bindings.write_to_file("libp8020.h");
+            },
+        );
 }
