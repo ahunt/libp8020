56a0fc3f4fb19df5a309de1e24f7130bdc177df2
Andrzej Hunt <andrzej@ahunt.org>
2024-11-09 15:42:31 +0100

    builtin configs: test that they can be loaded, and that they're valid

--- src/test_config/builtin/mod.rs
+++ src/test_config/builtin/mod.rs
@@ -3,3 +3,27 @@ pub const OSHA_LEGACY: &str = include_str!("osha_legacy.csv");
 pub const OSHA_FAST_FFP: &str = include_str!("osha_fast_ffp.csv");
 pub const OSHA_FAST_ELASTO: &str = include_str!("osha_fast_elasto.csv");
 pub const CRASH_2_5: &str = include_str!("crash_2_5.csv");
+
+const BUILTIN_CONFIGS: [&str; 5] = [
+    OSHA,
+    OSHA_LEGACY,
+    OSHA_FAST_FFP,
+    OSHA_FAST_ELASTO,
+    CRASH_2_5,
+];
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::test_config::TestConfig;
+
+    #[test]
+    fn test_BUILTIN_CONFIGS_load_and_validate() {
+        for config in BUILTIN_CONFIGS {
+            let mut cursor = std::io::Cursor::new(config.as_bytes());
+            let result = TestConfig::parse_from_csv(&mut cursor);
+            assert!(result.is_ok());
+            assert!(result.unwrap().validate().is_ok());
+        }
+    }
+}
