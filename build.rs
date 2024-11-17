cb7d4c58d0208694656d276e23cc87c37e844cbe
Andrzej Hunt <andrzej@ahunt.org>
2024-11-17 12:37:51 +0100

    cbindgen: enable usize_is_size_t
    This seems to be the de-facto recommended default, and avoids potentially
    awkward casting or warnings.

--- build.rs
+++ build.rs
@@ -4,14 +4,13 @@ use std::env;
 
 fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    cbindgen::generate(crate_dir)
-        .map_or_else(
-            |error| match error {
-                cbindgen::Error::ParseSyntaxError { .. } => {}
-                e => panic!("{:?}", e),
-            },
-            |bindings| {
-                bindings.write_to_file("libp8020.h");
-            },
-        );
+    cbindgen::generate(crate_dir).map_or_else(
+        |error| match error {
+            cbindgen::Error::ParseSyntaxError { .. } => {}
+            e => panic!("{:?}", e),
+        },
+        |bindings| {
+            bindings.write_to_file("libp8020.h");
+        },
+    );
 }
