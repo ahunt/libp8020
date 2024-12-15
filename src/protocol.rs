4f1b39f9a0c2e550aa5f8d8a34a9727bcd134486
Andrzej Hunt <andrzej@ahunt.org>
2024-12-15 08:36:24 +0100

    clippy: address "warning: manual check for common ascii range"
    warning: manual check for common ascii range
       --> src/protocol.rs:253:24
        |
    253 |         ref message if matches!(message.chars().next().unwrap_or('x'), '0'..='9') => {
        |                        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ help: try: `message.chars().next().unwrap_or('x').is_ascii_digit()`
        |
        = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#manual_is_ascii_check
        = note: `#[warn(clippy::manual_is_ascii_check)]` on by default

--- src/protocol.rs
+++ src/protocol.rs
@@ -250,7 +250,7 @@ pub fn parse_message(message: &str) -> Result<Message, ParseError> {
         // back here if command parsing fails.
         // TODO: consider checking length too - the specs claim this will always be 9
         // chars long.
-        ref message if matches!(message.chars().next().unwrap_or('x'), '0'..='9') => {
+        ref message if message.chars().next().unwrap_or('x').is_ascii_digit() => {
             match f64::from_str(message) {
                 Ok(sample) => Ok(Message::Sample(sample)),
                 Err(_) => Err(ParseError {
