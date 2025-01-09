f0a35bc51baf9bc1d0fb79eb483c01cf49265f86
Andrzej Hunt <andrzej@ahunt.org>
2025-01-09 19:08:44 +0100

    particle-reader: migrate to use libp8020

--- src/bin/particle-reader.rs
+++ src/bin/particle-reader.rs
@@ -1,61 +1,62 @@
-extern crate serialport;
-use std::io::BufRead;
+use std::env;
+use std::sync::mpsc;
 
-// TODO: enumerate devices dynamically
-const DEVICE: &str = "/dev/ttyUSB0";
-
-fn send(port: &mut Box<dyn serialport::SerialPort>, msg: &str) {
-    if !msg.is_ascii() {
-        eprintln!("Unexpected non-ascii msg: {}", msg);
-        // TODO: switch to proper error handling.
-        std::process::exit(0);
-    }
-
-    let mut len_written = port.write(msg.as_bytes()).unwrap();
-    len_written += port.write(b"\r").unwrap();
-    if len_written != (msg.len() + 1) {
-        eprintln!(
-            "Expected to write {} bytes, actually wrote {}.",
-            msg.len() + 1,
-            len_written
-        );
-        std::process::exit(0);
-    }
-}
+use p8020::{Device, DeviceNotification};
 
 fn main() {
-    eprintln!(
-        "P8020A reader binary (v{}). (Please note: all I can do is log raw data.)",
-        env!("CARGO_PKG_VERSION")
-    );
+    eprintln!("P8020A reader binary (v{})", env!("CARGO_PKG_VERSION"));
 
-    // See "PortaCount Plus Model 8020 Technical Addendum" for specs.
-    // Note: baud is configurable on the devices itself, 1200 is the default.
-    let mut port = serialport::new(DEVICE, /* baud_rate */ 1200)
-        .data_bits(serialport::DataBits::Eight)
-        .parity(serialport::Parity::None)
-        .stop_bits(serialport::StopBits::One)
-        .timeout(core::time::Duration::new(15, 0))
-        .open()
-        .expect("Unable to open serial port, sorry");
-
-    send(&mut port, "J");
-
-    let reader = std::io::BufReader::new(port);
-
-    for line in reader.lines() {
-        let date_time = time::OffsetDateTime::now_utc();
-        let format = time::macros::format_description!(
-            version = 2,
-            "[year]-[month]-[day]T[hour]:[minute]:[second]"
-        );
-        let formatted_date_time = date_time.format(&format).unwrap();
-
-        // Note: will contain trailing CR (8020A sends CR+LF, BufReader removes the LF).
-        // println!("Received: {} @ {}", line.unwrap(), formatted_date_time);
-        println!("{},{}", formatted_date_time, line.unwrap().trim());
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("\nusage: particle-reader <device>");
+        return;
     }
+    let path = &args[1];
 
-    // TODO: check N95 companion.
-    // TODO: release from external control on exit.
+    let (tx_connection_closed, rx_done) = mpsc::channel();
+    let tx_request_exit = tx_connection_closed.clone();
+
+    let callback = move |notification: DeviceNotification| {
+        match notification {
+            DeviceNotification::Sample { particle_conc } => {
+                let date_time = time::OffsetDateTime::now_utc();
+                let format = time::macros::format_description!(
+                    version = 2,
+                    "[year]-[month]-[day]T[hour]:[minute]:[second]"
+                );
+                let formatted_date_time = date_time.format(&format).unwrap();
+                println!("{},{}", formatted_date_time, particle_conc);
+            }
+            DeviceNotification::ConnectionClosed => {
+                tx_connection_closed.send(()).unwrap();
+            }
+            DeviceNotification::DeviceProperties(properties) => {
+                // Slight race condition: this might arrive _after_ the first
+                // datapoint if the device was already in external control mode.
+                eprintln!(
+                    "8020(A): #{0} (last serviced: {1}-{2}, runtime since last service: {3})",
+                    properties.serial_number,
+                    properties.last_service_year,
+                    properties.last_service_month,
+                    properties.run_time_since_last_service_hours as usize
+                );
+            }
+            _ => (),
+        };
+    };
+
+    ctrlc::set_handler(move || {
+        tx_request_exit.send(()).unwrap();
+    })
+    .unwrap();
+
+    match Device::connect_path(path, Some(callback)) {
+        // _device must be kept alive to keep the connection alive.
+        Ok(_device) => {
+            rx_done.recv().expect("rx_done failed");
+        }
+        Err(e) => {
+            eprintln!("Failed to connect to device: {e}");
+        }
+    }
 }
